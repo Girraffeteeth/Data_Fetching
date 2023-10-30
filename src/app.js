bcfbb4c0022c473804e9577ae9755c298c42a875
Meghna Das <gdyes128@gmail.com>
2023-10-30 11:30:54 +0530

    CHANGES MADE

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import './app.css';
+import Details from './components/details';
+
+function App() {
+  return (
+    <div className="app">
+      <Details />
+    </div>
+  );
+}
+
+export default App;
